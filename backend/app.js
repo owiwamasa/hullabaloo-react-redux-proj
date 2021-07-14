17ffd69b51812800f2f0b1b66eac463008dc0db6
Owen Iwamasa <owiwamasa@gmail.com>
2021-07-14 18:24:47 -0400

    phase 2 done

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { environment } = require('./config')
 const routes = require('./routes')
+const { ValidationError } = require('sequelize')
 
 const isProduction = environment === 'production'
 
@@ -32,7 +33,33 @@ app.use(
         }
     })
 )
-
-
 app.use(routes)
+
+app.use((_req, _res, next) => {
+    const err = new Error('The requested resource could not be found.')
+    err.title = 'Resource Not Found'
+    err.errors = ['The requested resource could not be found.']
+    err.status = 404
+    next(err)
+})
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message)
+        err.title = 'Validation error'
+    }
+    next(err);
+})
+
+app.use((err, _req, _res, _next) => {
+    _res.status(err.status || 500)
+    console.error(err)
+    _res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    })
+})
+
 module.exports = app
