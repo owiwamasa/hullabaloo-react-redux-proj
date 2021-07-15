2b80916089bd3d9f48398c8c9486cbcd94ddcb10
Owen Iwamasa <owiwamasa@gmail.com>
2021-07-15 08:50:24 -0400

    phase 4 done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -26,6 +26,25 @@ router.post('/', asyncHandler(async (req, res, next) => {
     })
 }))
 
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
+    }
+);
+
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req
+    if (user) {
+        return res.json({
+            user: user.toSafeObject()
+        })
+    } else {
+        return res.json({})
+    }
+})
+
 
 
 module.exports = router
