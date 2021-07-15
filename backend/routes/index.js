2df430eeafcc2383baa215acb7c51979e375e753
Owen Iwamasa <owiwamasa@gmail.com>
2021-07-15 12:41:45 -0400

    completed front-end set up

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,5 +4,31 @@ const apiRouter = require('./api')
 
 router.use('/api', apiRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path')
+
+    router.get('/', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken())
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        )
+    })
+    router.use(express.static(path.resolve('../frontend/build')))
+
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken())
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        )
+    })
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken())
+        return res.json({})
+    })
+}
+
 
 module.exports = router;
